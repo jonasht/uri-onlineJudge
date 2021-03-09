df2db1ccd0c0c3e55bc6b929b28e1c942160c31c
jonasht <jhenriquet@outlook.com.br>
2021-03-09 19:26:41 -0300

    add

--- 1-primeiro/2-comPoo.py
+++ 1-primeiro/2-comPoo.py
@@ -19,19 +19,25 @@ class loja:
     
 
 def enfeite(): print('=-'*30+'=')
-# antigo preço
+
+
+# colocando o preços errados
 a = loja(notebook='137,00', mochila='3738,00')
 
+
 enfeite()
+
+# mostrando os preços antigos na tela 
 print('preços antigos:')
 print(f'notebook: {a.get_notebook()}')
 print(f'mochila: {a.get_mochila()}')
 enfeite()
 
+# arrumando os preços corretos
 guardar = a.get_notebook()
 a.set_notebook(a.get_mochila())
 a.set_mochila(guardar)
 
-
+# mostrando os preços atualizados na tela 
 print(f'preços atualizados:\nnotebook:{a.get_notebook()} mochila:{a.get_mochila()}')
 enfeite()
